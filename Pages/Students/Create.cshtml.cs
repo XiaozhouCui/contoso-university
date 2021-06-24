415508814dcb1caa8e030fe1587cc3a014867082
Joe Cui <joe.cui@bpanz.com>
2021-06-24 16:06:58 +1000

    Update the database context class

--- Pages/Students/Create.cshtml.cs
+++ Pages/Students/Create.cshtml.cs
@@ -35,7 +35,7 @@ namespace ContosoUniversity.Pages.Students
                 return Page();
             }
 
-            _context.Student.Add(Student);
+            _context.Students.Add(Student);
             await _context.SaveChangesAsync();
 
             return RedirectToPage("./Index");
