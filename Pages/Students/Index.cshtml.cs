fe19bf0a0bf224062faba3b54fa46a009bd07ddb
Joe Cui <joe.cui@bpanz.com>
2021-06-28 10:20:20 +1000

    Add sorting in IndexModel

--- Pages/Students/Index.cshtml.cs
+++ Pages/Students/Index.cshtml.cs
@@ -12,18 +12,49 @@ namespace ContosoUniversity.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly ContosoUniversity.Data.SchoolContext _context;
-
-        public IndexModel(ContosoUniversity.Data.SchoolContext context)
+        private readonly SchoolContext _context;
+        public IndexModel(SchoolContext context)
         {
             _context = context;
         }
 
-        public IList<Student> Student { get;set; }
+        // Add properties to contain the sorting parameters
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public IList<Student> Students { get; set; }
+
+        // sortOrder comes from query string, it is either Name or Date
+        // ?sortOrder=... will be added to query string when the user clicks a column heading link
+        public async Task OnGetAsync(string sortOrder)
         {
-            Student = await _context.Students.ToListAsync();
+            // NameSort and DateSort are used by the Razor Page to configure the column heading hyperlinks
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : ""; // using System
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+
+            // When an IQueryable studentsIQ is created or modified, no query is sent to the database (deferred)
+            IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+            // modify the IQueryable before execution
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate); // ascending order by date
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName); // default: ascending order by last name
+                    break;
+            }
+            // the deferred IQueryable studentsIQ is finally executed after .ToListAsync()
+            Students = await studentsIQ.AsNoTracking().ToListAsync();
         }
     }
 }
