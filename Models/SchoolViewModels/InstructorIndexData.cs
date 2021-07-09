6c025a58ff9ef5031619c84ab37adf5ffa278d34
Joe Cui <joe.cui@bpanz.com>
2021-07-09 10:45:57 +1000

    Create a view model for instructors page, showing data from three different tables

---
+++ Models/SchoolViewModels/InstructorIndexData.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    // The instructors page shows data from three different tables. A view model is needed that includes three properties representing the three tables.
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
--- Pages/Courses/Index.cshtml.cs
+++ Pages/Courses/Index.cshtml.cs
@@ -18,10 +18,11 @@ namespace ContosoUniversity.Pages.Courses
         public IList<Course> Courses { get; set; }
 
         // The scaffolding engine specified eager loading for the Department navigation property
+        // Eager loading is typically more efficient when the related data needs to be displayed
         public async Task OnGetAsync()
         {
             Courses = await _context.Courses
-                .Include(c => c.Department) // .Include() specifies eager loading
+                .Include(c => c.Department) // .Include() specifies eager loading, an alternative is .Select() from LINQ
                 .AsNoTracking() // improves performance because the entities returned are not tracked, not tracked because they're not updated in the current context.
                 .ToListAsync();
         }
