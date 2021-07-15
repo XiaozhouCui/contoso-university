d119c084ed42e8d9b36f85f5cd833bdc81e0892b
Joe Cui <joe.cui@bpanz.com>
2021-07-15 11:28:46 +1000

    Update Instructor Create page to add checkboxes

--- Pages/Instructors/Create.cshtml.cs
+++ Pages/Instructors/Create.cshtml.cs
@@ -1,44 +1,89 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using ContosoUniversity.Data;
 using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ContosoUniversity.Pages.Instructors
 {
-    public class CreateModel : PageModel
+    public class CreateModel : InstructorCoursesPageModel
     {
         private readonly ContosoUniversity.Data.SchoolContext _context;
+        private readonly ILogger<InstructorCoursesPageModel> _logger;
 
-        public CreateModel(ContosoUniversity.Data.SchoolContext context)
+        // inject db context and logger
+        public CreateModel(SchoolContext context, ILogger<InstructorCoursesPageModel> logger)
         {
             _context = context;
+            _logger = logger; // Add logging for warning and error messages.
         }
 
         public IActionResult OnGet()
         {
+            var instructor = new Instructor();
+            instructor.Courses = new List<Course>();
+
+            // Provides an empty collection for the foreach loop
+            // foreach (var course in Model.AssignedCourseDataList)
+            // in the Create Razor page.
+            PopulateAssignedCourseData(_context, instructor);
             return Page();
         }
 
-        [BindProperty]
+        [BindProperty] // bind razor page with model?
         public Instructor Instructor { get; set; }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string[] selectedCourses)
         {
-            if (!ModelState.IsValid)
+            var newInstructor = new Instructor();
+
+            if (selectedCourses.Length > 0)
             {
-                return Page();
+                newInstructor.Courses = new List<Course>();
+                // Load() fetches all the Courses in one database call
+                _context.Courses.Load();
             }
 
-            _context.Instructors.Add(Instructor);
-            await _context.SaveChangesAsync();
+            // Add selected Courses courses to the new instructor.
+            foreach (var course in selectedCourses)
+            {
+                // FindAsync returns the tracked entity without a request to the database. This is an optimization for small connections
+                var foundCourse = await _context.Courses.FindAsync(int.Parse(course));
+                if (foundCourse != null)
+                {
+                    newInstructor.Courses.Add(foundCourse);
+                }
+                else
+                {
+                    _logger.LogWarning("Course {course} not found", course);
+                }
+            }
 
-            return RedirectToPage("./Index");
+            try
+            {
+                if (await TryUpdateModelAsync<Instructor>(
+                                newInstructor,
+                                "Instructor",
+                                i => i.FirstMidName, i => i.LastName,
+                                i => i.HireDate, i => i.OfficeAssignment))
+                {
+                    // DbSet<T>.Add() creates a new Instructor using many-to-many relationships without explicitly mapping the join table.
+                    _context.Instructors.Add(newInstructor);
+                    await _context.SaveChangesAsync();
+                    return RedirectToPage("./Index");
+                }
+                return RedirectToPage("./Index");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+            }
+
+            PopulateAssignedCourseData(_context, Instructor);
+            return Page();
         }
     }
 }
