fab480df098ce0e84522edaf673db5e7300fa4aa
Joe Cui <joe.cui@bpanz.com>
2021-08-11 08:59:46 +1000

    Update the Index page of Departments

--- Utility.cs
+++ Utility.cs
@@ -1,4 +1,4 @@
-#if SQLiteVersion
+// #if SQLiteVersion
 // #if SQLiteVersion is a "preprocessor directive", isolates the differences in the SQLite and SQL Server
 using System;
 
@@ -12,16 +12,16 @@ namespace ContosoUniversity
         }
     }
 }
-#else
-// The author maintain one code base for both versions.
-namespace ContosoUniversity
-{
-    public static class Utility
-    {
-        public static string GetLastChars(byte[] token)
-        {
-            return token[7].ToString();
-        }
-    }
-}
-#endif
+// #else
+// // The author maintain one code base for both versions.
+// namespace ContosoUniversity
+// {
+//     public static class Utility
+//     {
+//         public static string GetLastChars(byte[] token)
+//         {
+//             return token[7].ToString();
+//         }
+//     }
+// }
+// #endif
